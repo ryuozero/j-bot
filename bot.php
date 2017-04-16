ca4a7be2c137895b7859ba249443a718d8d9e298
janwit trisakun <junta753@hotmail.com>
2017-04-16 09:56:02 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -87,7 +87,7 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['messages'][0]['type'] = "video";
   $arrPostData['messages'][0]['originalContentUrl'] = "https://krujunrmu.com/img/ff.mp4";
   $arrPostData['messages'][0]['previewImageUrl'] = "https://krujunrmu.com/img/ff.jpg";	
-}else if($arrJson['events'][0]['message']['text'] == "หวย"){
+}else if($arrJson['events'][0]['message']['text'] == "หวย" || $arrJson['events'][0]['message']['text'] == "ตรวจฉลาก" ){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
